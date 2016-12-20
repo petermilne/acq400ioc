9811a02ea4d91ad6de4ff6b1eedbb403059500fe
Peter Milne <peter.milne@d-tacq.com>
2016-12-20 21:06:24 +0000

    fix links 	modified:   scripts/acq2106_init.sh

--- scripts/acq2106_init.sh
+++ scripts/acq2106_init.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 if [ "x$1" = "xcreate" ]; then
-	ln -s /usr/local/epics/acq2106_set_ext_clk /etc/acq400/0
-	ln -s /usr/local/epics/acq2106_set_int_clk /etc/acq400/0
+	ln -s /usr/local/epics/scripts/acq2106_set_ext_clk /etc/acq400/0
+	ln -s /usr/local/epics/scripts/acq2106_set_int_clk /etc/acq400/0
 	exit 0
 fi
 
