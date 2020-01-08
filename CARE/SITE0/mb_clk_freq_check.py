fa8e6edc3ee1a407dce99aff75b25b0d34cde655
Sean <sean.alsop@d-tacq.co.uk>
2020-01-08 12:30:12 +0000

    Added a callout to a python script that checks that fs has been set correctly. 	new file:   mb_clk_freq_check.py 	modified:   sync_role.common          Added a call inside sync_role so that we can use          python to check the desired frequency was set correctly.          This is useful because one in twenty times the system          would not set the correct frequency on boot.

---
+++ CARE/SITE0/mb_clk_freq_check.py
@@ -0,0 +1,30 @@
+import subprocess
+import time
+import sys
+
+def get_freq(fs_desired):
+    freq_read = subprocess.check_output(["get.site","0", "SIG:CLK_MB:FREQ"])
+    freq_read = float(freq_read[:-1].split(" ")[1])
+    setpt_write = subprocess.check_output(["set.site","0", "SIG:CLK_MB:SET",str(fs_desired)])
+    setpt_read = subprocess.check_output(["set.site","0", "SIG:CLK_MB:SET"])
+    setpt_read = setpt_read[:-1].split(" ")[1]
+    return freq_read,setpt_read
+
+
+fs_desired = float(sys.argv[1]) # Always pass desired freq setpoint from sync_role call
+less_1pct = fs_desired - (0.01*fs_desired)
+plus_1pct = fs_desired + (0.01*fs_desired)
+
+freq_read,setpt_read = get_freq(fs_desired)
+
+
+while True:
+    if less_1pct <= freq_read <= plus_1pct :
+        break
+    else:
+        time.sleep(0.5)
+        print "\nClock Monitor Catch :: Setpoint = {}, MB_CLK Output = {}".format(fs_desired,freq_read)
+        print "Thresholds {} {}".format(less_1pct,plus_1pct)
+        print "Resetting MB_CLK ",fs_desired
+        freq_read,setpt_read = get_freq(fs_desired)
+
