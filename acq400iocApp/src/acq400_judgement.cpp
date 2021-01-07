bf6ca539f244c0c27f5d8db175ca664353039465
Peter Milne <peter.milne@d-tacq.com>
2021-01-07 16:18:10 +0000

    judgement .. very very basic skeleton compiles..  On branch judgement 	modified:   acq400iocApp/src/Makefile 	new file:   acq400iocApp/src/acq400_judgement.cpp 	new file:   acq400iocApp/src/acq400_judgement.h 	modified:   configure/RELEASE 	new file:   dbd/acq400_judgement.dbd

---
+++ acq400iocApp/src/acq400_judgement.cpp
@@ -0,0 +1,139 @@
+/*
+ * acq400_judgement.cpp : compares incoming waveforms against Mask Upper MU, Mask Lower ML and reports FAIL if outside mask
+ *
+ *  Created on: 7 Jan 2021
+ *      Author: pgm
+ */
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <errno.h>
+#include <math.h>
+
+#include <epicsTypes.h>
+#include <epicsTime.h>
+#include <epicsThread.h>
+#include <epicsString.h>
+#include <epicsTimer.h>
+#include <epicsMutex.h>
+#include <epicsEvent.h>
+#include <iocsh.h>
+
+#include "acq400_judgement.h"
+#include <epicsExport.h>
+
+
+#define MAX_ENUM_STRING_SIZE 20
+
+
+static const char *driverName="acq164AsynPortDriver";
+
+acq400Judgement::acq400Judgement(const char* portName, int _nchan, int _nsam):
+		asynPortDriver(portName,
+		                    _nchan, 														/* maxAddr */
+		                    asynInt32Mask | asynFloat64Mask | asynFloat64ArrayMask | asynEnumMask | asynInt8ArrayMask | asynDrvUserMask, /* Interface mask */
+		                    asynInt32Mask | asynFloat64Mask | asynFloat64ArrayMask | asynEnumMask | asynInt8ArrayMask,  				 /* Interrupt mask */
+		                    0, /* asynFlags.  This driver does not block and it is not multi-device, so flag is 0 */
+		                    1, /* Autoconnect */
+		                    0, /* Default priority */
+		                    0) /* Default stack size*/,
+		nchan(_nchan), nsam(_nsam)
+{
+	createParam(PS_NCHAN,                asynParamInt32,         &P_NCHAN);
+	createParam(PS_NSAM,                 asynParamInt32,         &P_NSAM);
+	createParam(PS_MASK_FROM_DATA,       asynParamInt32,         &P_MASK_FROM_DATA);
+	createParam(PS_MU,                   asynParamInt16Array,    &P_MU);
+	createParam(PS_ML,                   asynParamInt16Array,    &P_ML);
+	createParam(PS_RAW,                  asynParamInt16Array,    &P_RAW);
+
+	setIntegerParam(P_NCHAN, 			nchan);
+	setIntegerParam(P_NSAM, 			nsam);
+	setIntegerParam(P_MASK_FROM_DATA, 	0);
+
+	RAW_MU = new epicsInt16[nsam*nchan];
+	RAW_ML = new epicsInt16[nsam*nchan];
+	RAW = 0;   // get va from BUFFER
+}
+
+/** Called when asyn clients call pasynInt32->write().
+  * This function sends a signal to the simTask thread if the value of P_Run has changed.
+  * For all parameters it sets the value in the parameter library and calls any registered callbacks..
+  * \param[in] pasynUser pasynUser structure that encodes the reason and address.
+  * \param[in] value Value to write. */
+asynStatus acq400Judgement::writeInt32(asynUser *pasynUser, epicsInt32 value)
+{
+    int function = pasynUser->reason;
+    asynStatus status = asynSuccess;
+    const char *paramName;
+    const char* functionName = "writeInt32";
+
+    /* Set the parameter in the parameter library. */
+    status = (asynStatus) setIntegerParam(function, value);
+
+    /* Fetch the parameter string name for possible use in debugging */
+    getParamName(function, &paramName);
+
+    if (function == P_MASK_FROM_DATA) {
+    	/* make mask from data */
+    } else {
+        /* All other parameters just get set in parameter list, no need to
+         * act on them here */
+    }
+
+    /* Do callbacks so higher layers see any changes */
+    status = (asynStatus) callParamCallbacks();
+
+    if (status)
+        epicsSnprintf(pasynUser->errorMessage, pasynUser->errorMessageSize,
+                  "%s:%s: status=%d, function=%d, name=%s, value=%d",
+                  driverName, functionName, status, function, paramName, value);
+    else
+        asynPrint(pasynUser, ASYN_TRACEIO_DRIVER,
+              "%s:%s: function=%d, name=%s, value=%d\n",
+              driverName, functionName, function, paramName, value);
+    return status;
+}
+
+
+int acq400Judgement::factory(const char *portName, int maxPoints, int nchan)
+{
+    	new acq400Judgement(portName, maxPoints, nchan);
+
+    	return(asynSuccess);
+}
+
+
+/* Configuration routine.  Called directly, or from the iocsh function below */
+
+extern "C" {
+
+/** EPICS iocsh callable function to call constructor for the testAsynPortDriver class.
+  * \param[in] portName The name of the asyn port driver to be created.
+  * \param[in] maxPoints The maximum  number of points in the volt and time arrays */
+int acq400JudgementConfigure(const char *portName, int maxPoints, int nchan)
+{
+	return acq400Judgement::factory(portName, maxPoints, nchan);
+}
+
+
+/* EPICS iocsh shell commands */
+
+static const iocshArg initArg0 = { "portName",iocshArgString};
+static const iocshArg initArg1 = { "max points",iocshArgInt};
+static const iocshArg initArg2 = { "max chan",iocshArgInt};
+static const iocshArg * const initArgs[] = {&initArg0, &initArg1, &initArg2};
+static const iocshFuncDef initFuncDef = {"acq400JudgementConfigure",3,initArgs};
+static void initCallFunc(const iocshArgBuf *args)
+{
+	acq400JudgementConfigure(args[0].sval, args[1].ival, args[2].ival);
+}
+
+void acq400_judgementRegister(void)
+{
+    iocshRegister(&initFuncDef,initCallFunc);
+}
+
+epicsExportRegistrar(acq400_judgementRegister);
+
+}
---
+++ acq400iocApp/src/acq400_judgement.h
@@ -0,0 +1,48 @@
+/*
+ * acq400_judgement.h
+ *
+ *  Created on: 7 Jan 2021
+ *      Author: pgm
+ */
+
+#ifndef ACQ400IOCAPP_SRC_ACQ400_JUDGEMENT_H_
+#define ACQ400IOCAPP_SRC_ACQ400_JUDGEMENT_H_
+
+
+#include "asynPortDriver.h"
+
+#define PS_NCHAN "NCHAN"							/* asynInt32, 		r/o */
+#define PS_NSAM	"NSAM"								/* asynInt32,       r/o */
+#define PS_MASK_FROM_DATA "MAKE_MASK_FROM_DATA"		/* asynInt32,       r/w .. MU=y+val, ML=y-val */
+#define PS_MU	"MASK_UPPER"						/* asynInt16Array   r/w */
+#define PS_ML	"MASK_LOWER"						/* asynInt16Array   r/w */
+#define PS_RAW	"RAW"								/* asynInt16Array   r/o */
+
+#define P_JUDGEMENT_RESULT "JUDGEMENT_RESULT"		/* asynInt8Array[NCHAN] r/o */
+
+class acq400Judgement: public asynPortDriver {
+public:
+	virtual asynStatus writeInt32(asynUser *pasynUser, epicsInt32 value);
+
+	static int factory(const char *portName, int maxPoints, int nchan);
+
+protected:
+	acq400Judgement(const char* portName, int nchan, int nsam);
+
+	const int nchan;
+	const int nsam;
+    /** Values used for pasynUser->reason, and indexes into the parameter library. */
+    int P_NCHAN;
+    int P_NSAM;
+    int P_MASK_FROM_DATA;
+    int P_MU;
+    int P_ML;
+    int P_RAW;
+
+    /* our data */
+    epicsInt16* RAW_MU;
+    epicsInt16* RAW_ML;
+    epicsInt16* RAW;
+};
+
+#endif /* ACQ400IOCAPP_SRC_ACQ400_JUDGEMENT_H_ */
